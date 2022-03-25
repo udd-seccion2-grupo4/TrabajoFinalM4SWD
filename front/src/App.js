507250d7ffbc8b303bfdb28d4358e722ff37ed84
gamboa <ngamboa@insacom.cl>
2022-03-25 17:29:48 -0300

    feat: agrega front para los distintos servicios

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import { Form, Input, Button, Row, Col, Space, Descriptions } from 'antd';
+import { Form, Input, Button, Row, Col, Space, Descriptions, Radio } from 'antd';
 import Paragraph from 'antd/lib/typography/Paragraph';
 import Title from 'antd/lib/typography/Title';
 import { useState } from 'react';
@@ -7,13 +7,15 @@ export const App = () => {
 
   const [isLoading, setLoading] = useState(false);
   const [response, setResponse] = useState(null);
+  const [operacion, setOperacion] = useState(null);
   const [error, setError] = useState(null);
 
-  const onFinish = (values) => {
+  const onSubmit = (values) => {
     setLoading(true);
     setResponse(null);
     setError(null);
-    fetch(`/rest/msdxc/dxc?sueldo=${values.sueldo}&ahorro=${values.ahorro}`)
+    setOperacion(values.operacion);
+    fetch(`/rest/msdxc/${values.operacion}?sueldo=${values.sueldo}&ahorro=${values.ahorro}`)
       .then(response => {
         if (!response.ok) {
           return Promise.reject(response.status);
@@ -35,9 +37,9 @@ export const App = () => {
     <Row>
       <Col span={12} offset={6}>
         <Space direction='vertical' style={{ marginTop: 20, width: '100%' }} >
-          <Title>Consulta tu 10 %</Title>
-          <Form name="consultar" layout='vertical'
-            onFinish={onFinish}
+          <Title>Calculadora 10%</Title>
+          <Form name="calculadora" layout='vertical'
+            onFinish={onSubmit}
             autoComplete="off">
 
             <Form.Item label="Sueldo" name="sueldo"
@@ -50,23 +52,74 @@ export const App = () => {
               <Input size='large' placeholder='Ingresar Ahorro' type={'number'} />
             </Form.Item>
 
-            <Form.Item>
-              <Button size='large' type="primary" loading={isLoading} htmlType="submit">Consultar 10%</Button>
+            <Form.Item label="Qué desea hacer?" name="operacion"
+              rules={[{ required: true, message: 'Debe seleccionar la operación.' }]}>
+                <Radio.Group buttonStyle="outline">
+                  <Radio.Button value="dxc">Calcular 10%</Radio.Button>
+                  <Radio.Button value="impuesto">Calcular Impuesto</Radio.Button>
+                  <Radio.Button value="saldo">Calcular Saldo después del Retiro</Radio.Button>
+                  <Radio.Button value="all">Simular Todo</Radio.Button>
+                </Radio.Group>
             </Form.Item>
-
+            <Button size='large' type="primary" loading={isLoading} htmlType="submit">Consultar</Button>
           </Form>
         </Space>
       </Col>
-      {response ?
+      {response && operacion === 'all' ?
         <Col span={12} offset={6}>
           <Space direction='vertical' style={{ marginTop: 20, marginBottom: 20, width: '100%' }} >
             <Descriptions
               bordered
-              title="Resultado"
+              title="Resultado Simulación"
             >
               <Descriptions.Item label="10 %"><span id="calc_dxc">$ {Intl.NumberFormat('es-CL').format(response.dxc)}</span></Descriptions.Item>
               <Descriptions.Item label="Saldo"><span id="calc_saldo">$ {Intl.NumberFormat('es-CL').format(response.saldo)}</span></Descriptions.Item>
               <Descriptions.Item label="Impuesto"><span id="calc_impuesto">$ {Intl.NumberFormat('es-CL').format(response.impuesto)}</span></Descriptions.Item>
+              <Descriptions.Item label="Sueldo"><span id="calc_sueldo">$ {Intl.NumberFormat('es-CL').format(response.sueldo)}</span></Descriptions.Item>
+              <Descriptions.Item label="Ahorro"><span id="calc_ahorro">$ {Intl.NumberFormat('es-CL').format(response.ahorro)}</span></Descriptions.Item>
+              <Descriptions.Item label="UF"><span id="calc_uf">$ {Intl.NumberFormat('es-CL').format(response.uf)}</span></Descriptions.Item>
+            </Descriptions>
+          </Space>
+        </Col>
+        : null}
+      {response && operacion === 'dxc' ?
+        <Col span={12} offset={6}>
+          <Space direction='vertical' style={{ marginTop: 20, marginBottom: 20, width: '100%' }} >
+            <Descriptions
+              bordered
+              title="Resultado Calcular 10%"
+            >
+              <Descriptions.Item label="10 %"><span id="calc_dxc">$ {Intl.NumberFormat('es-CL').format(response.dxc)}</span></Descriptions.Item>
+              <Descriptions.Item label="Sueldo"><span id="calc_sueldo">$ {Intl.NumberFormat('es-CL').format(response.sueldo)}</span></Descriptions.Item>
+              <Descriptions.Item label="Ahorro"><span id="calc_ahorro">$ {Intl.NumberFormat('es-CL').format(response.ahorro)}</span></Descriptions.Item>
+              <Descriptions.Item label="UF"><span id="calc_uf">$ {Intl.NumberFormat('es-CL').format(response.uf)}</span></Descriptions.Item>
+            </Descriptions>
+          </Space>
+        </Col>
+        : null}
+      {response && operacion === 'impuesto' ?
+        <Col span={12} offset={6}>
+          <Space direction='vertical' style={{ marginTop: 20, marginBottom: 20, width: '100%' }} >
+            <Descriptions
+              bordered
+              title="Resultado Calcular Impuesto"
+            >
+              <Descriptions.Item label="Impuesto"><span id="calc_impuesto">$ {Intl.NumberFormat('es-CL').format(response.impuesto)}</span></Descriptions.Item>
+              <Descriptions.Item label="Sueldo"><span id="calc_sueldo">$ {Intl.NumberFormat('es-CL').format(response.sueldo)}</span></Descriptions.Item>
+              <Descriptions.Item label="Ahorro"><span id="calc_ahorro">$ {Intl.NumberFormat('es-CL').format(response.ahorro)}</span></Descriptions.Item>
+              <Descriptions.Item label="UF"><span id="calc_uf">$ {Intl.NumberFormat('es-CL').format(response.uf)}</span></Descriptions.Item>
+            </Descriptions>
+          </Space>
+        </Col>
+        : null}
+      {response && operacion === 'saldo' ?
+        <Col span={12} offset={6}>
+          <Space direction='vertical' style={{ marginTop: 20, marginBottom: 20, width: '100%' }} >
+            <Descriptions
+              bordered
+              title="Resultado Saldo después del Retiro"
+            >
+              <Descriptions.Item label="Saldo"><span id="calc_saldo">$ {Intl.NumberFormat('es-CL').format(response.saldo)}</span></Descriptions.Item>
               <Descriptions.Item label="Sueldo"><span id="calc_sueldo">$ {Intl.NumberFormat('es-CL').format(response.sueldo)}</span></Descriptions.Item>
               <Descriptions.Item label="Ahorro"><span id="calc_ahorro">$ {Intl.NumberFormat('es-CL').format(response.ahorro)}</span></Descriptions.Item>
               <Descriptions.Item label="UF"><span id="calc_uf">$ {Intl.NumberFormat('es-CL').format(response.uf)}</span></Descriptions.Item>
